c1755f50517848f0bb9f2e5eac07b3d3f5e0b517
백승진 <bsj1048@naver.com>
2017-06-10 23:12:09 +0900

    [13Á¶] evaluate ÇԼö ±¸Çö

--- src/main/java/kr/ac/cnu/games/poker/Evaluator.java
+++ src/main/java/kr/ac/cnu/games/poker/Evaluator.java
@@ -2,7 +2,10 @@ package kr.ac.cnu.games.poker;
 
 import org.springframework.stereotype.Component;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -17,7 +20,15 @@ public class Evaluator {
 
     public List<Hands> evalauteLowHands(List<Hands> handsList) {
         // do Something
-        return null;
+        Comparator<Hands> sort = new Comparator<Hands>() {
+            public int compare(Hands o1, Hands o2) {
+                return (o2.getHandsType().compareTo(o1.getHandsType()));
+            }
+        };
+        Collections.sort(handsList, sort);
+
+
+        return handsList;
     }
 
 }

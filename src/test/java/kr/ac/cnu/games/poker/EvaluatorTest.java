c660e1d77083ec8c4c1ea8360afb341b6d85bcd7
SOO HYUN KIM <skyfall13@naver.com>
2017-06-18 00:28:14 +0900

    Add Cardset in EvalustorTest.java

--- src/test/java/kr/ac/cnu/games/poker/EvaluatorTest.java
+++ src/test/java/kr/ac/cnu/games/poker/EvaluatorTest.java
@@ -67,6 +67,16 @@ public class EvaluatorTest {
         int result = evaluator.evalauteHandsType(myHands, otherHands);
         assertThat(result, is(1));
     }
+
+    @Test
+    public void extractorLowHands2_같은_원페어의_경우_후속_NUMBER_비교(){
+        //given
+       Hands myHands = getOnePair1();
+       Hands otherHans = getOnePair2();
+
+       int result = evaluator.evalauteHandsType(myHands, otherHans);
+       assertThat(result, is (1));
+    }
     // Low CARD 테스트 -->
 
     // Givens
@@ -92,4 +102,200 @@ public class EvaluatorTest {
         return new Hands(HandsType.STRIGHT, cardList);
     }
 
+    private Hands getOnePair1() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(2, Suit.SPADES));
+        cardList.add(new Card(2, Suit.HEARTS));
+        cardList.add(new Card(5, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(4, Suit.CLUBS));
+
+        return new Hands(HandsType.ONE_PAIR, cardList);
+    }
+
+    private Hands getOnePair2() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(2, Suit.CLUBS));
+        cardList.add(new Card(2, Suit.DIAMONDS));
+        cardList.add(new Card(5, Suit.CLUBS));
+        cardList.add(new Card(7, Suit.DIAMONDS));
+        cardList.add(new Card(4, Suit.CLUBS));
+
+        return new Hands(HandsType.ONE_PAIR, cardList);
+    }
+
+    private Hands getOnePair3() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(2, Suit.CLUBS));
+        cardList.add(new Card(2, Suit.DIAMONDS));
+        cardList.add(new Card(5, Suit.CLUBS));
+        cardList.add(new Card(7, Suit.DIAMONDS));
+        cardList.add(new Card(4, Suit.CLUBS));
+
+        return new Hands(HandsType.ONE_PAIR, cardList);
+    }
+
+    private Hands getTwoPair() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(2, Suit.CLUBS));
+        cardList.add(new Card(2, Suit.DIAMONDS));
+        cardList.add(new Card(5, Suit.SPADES));
+        cardList.add(new Card(5, Suit.DIAMONDS));
+        cardList.add(new Card(4, Suit.CLUBS));
+
+        return new Hands(HandsType.TWO_PAIR, cardList);
+    }
+
+    private Hands getTwoPair2() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(2, Suit.SPADES));
+        cardList.add(new Card(2, Suit.HEARTS));
+        cardList.add(new Card(5, Suit.CLUBS));
+        cardList.add(new Card(5, Suit.HEARTS));
+        cardList.add(new Card(4, Suit.DIAMONDS));
+
+        return new Hands(HandsType.TWO_PAIR, cardList);
+    }
+
+    private Hands getTripple() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(3, Suit.SPADES));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.CLUBS));
+        cardList.add(new Card(5, Suit.SPADES));
+        cardList.add(new Card(4, Suit.CLUBS));
+
+        return new Hands(HandsType.THREE_CARD, cardList);
+    }
+
+    private Hands getTripple2() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(5, Suit.SPADES));
+        cardList.add(new Card(6, Suit.HEARTS));
+        cardList.add(new Card(5, Suit.CLUBS));
+        cardList.add(new Card(5, Suit.DIAMONDS));
+        cardList.add(new Card(4, Suit.CLUBS));
+
+        return new Hands(HandsType.THREE_CARD, cardList);
+    }
+
+    private Hands getFullHouse() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(3, Suit.SPADES));
+        cardList.add(new Card(4, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.CLUBS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(4, Suit.CLUBS));
+
+        return new Hands(HandsType.FULL_HOUSE, cardList);
+    }
+
+    private Hands getFullHouse2() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.SPADES));
+        cardList.add(new Card(5, Suit.HEARTS));
+        cardList.add(new Card(1, Suit.CLUBS));
+        cardList.add(new Card(1, Suit.HEARTS));
+        cardList.add(new Card(5, Suit.CLUBS));
+
+        return new Hands(HandsType.FULL_HOUSE, cardList);
+    }
+
+    private Hands getFourCard() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.SPADES));
+        cardList.add(new Card(1, Suit.DIAMONDS));
+        cardList.add(new Card(1, Suit.CLUBS));
+        cardList.add(new Card(1, Suit.HEARTS));
+        cardList.add(new Card(5, Suit.CLUBS));
+
+        return new Hands(HandsType.FOUR_CARD, cardList);
+    }
+
+    private Hands getFourCard2() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(13, Suit.SPADES));
+        cardList.add(new Card(13, Suit.DIAMONDS));
+        cardList.add(new Card(13, Suit.CLUBS));
+        cardList.add(new Card(13, Suit.HEARTS));
+        cardList.add(new Card(7, Suit.DIAMONDS));
+
+        return new Hands(HandsType.FOUR_CARD, cardList);
+    }
+
+    private Hands StrightFlash() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.HEARTS));
+        cardList.add(new Card(2, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(4, Suit.HEARTS));
+        cardList.add(new Card(5, Suit.HEARTS));
+
+        return new Hands(HandsType.STRIGHT_FLUSH, cardList);
+    }
+
+    private Hands StrightFlash2() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.CLUBS));
+        cardList.add(new Card(5, Suit.CLUBS));
+        cardList.add(new Card(4, Suit.CLUBS));
+        cardList.add(new Card(2, Suit.CLUBS));
+        cardList.add(new Card(3, Suit.CLUBS));
+
+        return new Hands(HandsType.STRIGHT_FLUSH, cardList);
+    }
+
+    private Hands StrightFlash3() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(12, Suit.CLUBS));
+        cardList.add(new Card(1, Suit.CLUBS));
+        cardList.add(new Card(10, Suit.CLUBS));
+        cardList.add(new Card(13, Suit.CLUBS));
+        cardList.add(new Card(11, Suit.CLUBS));
+
+        return new Hands(HandsType.STRIGHT_FLUSH, cardList);
+    }
+
+    private Hands Noting() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.SPADES));
+        cardList.add(new Card(3, Suit.CLUBS));
+        cardList.add(new Card(7, Suit.HEARTS));
+        cardList.add(new Card(8, Suit.CLUBS));
+        cardList.add(new Card(9, Suit.DIAMONDS));
+
+        return new Hands(HandsType.NOTHING, cardList);
+    }
+    private Hands Noting2() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.DIAMONDS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(7, Suit.SPADES));
+        cardList.add(new Card(8, Suit.SPADES));
+        cardList.add(new Card(9, Suit.CLUBS));
+
+        return new Hands(HandsType.NOTHING, cardList);
+    }
+
+    private Hands Noting3() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.DIAMONDS));
+        cardList.add(new Card(2, Suit.HEARTS));
+        cardList.add(new Card(7, Suit.SPADES));
+        cardList.add(new Card(10, Suit.SPADES));
+        cardList.add(new Card(9, Suit.SPADES));
+
+        return new Hands(HandsType.NOTHING, cardList);
+    }
+
+    private Hands Noting4() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.DIAMONDS));
+        cardList.add(new Card(2, Suit.HEARTS));
+        cardList.add(new Card(7, Suit.DIAMONDS));
+        cardList.add(new Card(9, Suit.DIAMONDS));
+        cardList.add(new Card(10, Suit.HEARTS));
+
+        return new Hands(HandsType.NOTHING, cardList);
+    }
 }

1a27a36b2ac4de25601bf3accca73a830be157de
Carlcannotdance <hansmnh93@gmail.com>
2017-06-21 11:56:53 +0900

    [15조]Make it all work and add cases (#16)
    * Add todo commit
    
    * Make EvaluateHighHands functional
    
    * [15]Add evaluateHighHands
    
    * [15]Add evaluateHihands case
    
    * Add exception for nothing onepair twopair threecard and four cardf
    
    * flush testing
    
    * flush almost finish and add strtflush a little
    
    * Add exception for Full_house and cases
    
    * Add exception for Straight and Flush
    
    * [15]Make it all work and add cases

--- src/main/java/kr/ac/cnu/games/poker/Evaluator.java
+++ src/main/java/kr/ac/cnu/games/poker/Evaluator.java
@@ -17,12 +17,88 @@ public class Evaluator {
 
         Comparator<Hands> sort = new Comparator<Hands>() {
             public int compare(Hands o1, Hands o2) {
+
+                //handtype 이 같을 때
+                if(o1.getHandsType().compareTo(o2.getHandsType()) == 0){
+                    //원페어 투페어 쓰리카드 포카드 일 경우
+                    if(o1.getHandsType() == HandsType.ONE_PAIR || o1.getHandsType() == HandsType.TWO_PAIR || o1.getHandsType() == HandsType.THREE_CARD || o1.getHandsType() == HandsType.FOUR_CARD){
+                        return checkPairs(o1, o2);
+                    }
+
+                    //아무것도 없을 경우 큰 수자로 비교
+                    else if(o1.getHandsType() == HandsType.NOTHING){
+                        if(o1.getCardList().get(0).getNumber() > o2.getCardList().get(0).getNumber())
+                            return -1;
+                        else
+                            return 1;
+                    }
+
+                    //풀하우스일 경우 세개 숫자 크기 비교
+                    else if(o1.getHandsType() == HandsType.FULL_HOUSE) {
+                        return checkFullhouse(o1, o2);
+                    }
+
+                    //플러쉬일 경우 문자 비교
+                    else if(o1.getHandsType() == HandsType.FLUSH) {
+                        if (o1.getCardList().get(0).getSuit().compareTo(o2.getCardList().get(0).getSuit()) > 1)
+                            return 1;
+                        else
+                            return -1;
+                    }
+
+                    //스트레이트일경우 큰 숫자 비교
+                    else if(o1.getHandsType() == HandsType.STRIGHT) {
+                        if(o1.getCardList().get(0).getNumber() > o2.getCardList().get(0).getNumber())
+                            return -1;
+                        else
+                            return 1;
+                    }
+                }
+
+                return (o1.getHandsType().compareTo(o2.getHandsType()));
+            }
+        };
+
+        Collections.sort(handsList, sort);
+
+
+
+        return handsList;
+    }
+
+    private int checkFullhouse(Hands o1, Hands o2) {
+        int i = 0;
+        int j = 0;
+
+        while(o1.getCardList().get(i).getNumber() != o1.getCardList().get(i+1).getNumber()
+                || o1.getCardList().get(i+1).getNumber()!=o1.getCardList().get(i+2).getNumber()) { i++;}
+        while(o2.getCardList().get(j).getNumber() != o2.getCardList().get(j+1).getNumber()
+                || o2.getCardList().get(j+1).getNumber()!=o2.getCardList().get(j+2).getNumber()) { j++;}
+
+        if(o1.getCardList().get(i).getNumber() > o2.getCardList().get(j).getNumber())
+            return -1;
+        else
+            return 1;
+    }
+
+    private int checkPairs(Hands o1, Hands o2) {
+        int i = 0;
+        int j = 0;
+
+        while(o1.getCardList().get(i).getNumber()!=o1.getCardList().get(i+1).getNumber()) { i++;}
+        while(o2.getCardList().get(j).getNumber()!=o2.getCardList().get(j+1).getNumber()) { j++;}
+        if(o1.getCardList().get(i).getNumber() > o2.getCardList().get(j).getNumber())
+            return -1;
+        else
+            return 1;
+=======
                 return (o1.getHandsType().compareTo(o2.getHandsType()));
             }
         };
         Collections.sort(handsList, sort);
 
         return handsList;
+
     }
 
     public List<Hands> evalauteLowHands(List<Hands> handsList) {
@@ -38,5 +114,6 @@ public class Evaluator {
     }
 
 //    TODO
+
 
 }
--- src/test/java/kr/ac/cnu/games/poker/EvaluatorTest.java
+++ src/test/java/kr/ac/cnu/games/poker/EvaluatorTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -24,15 +25,102 @@ public class EvaluatorTest {
     // <!-- High CARD 테스트
 
     @Test
+    //handstype이 다를 때 큰 것 부터 정렬
     public void evaluateHighCard1() {
         List<Hands> handsList = new ArrayList<>();
-        handsList.add(getStright());
-        handsList.add(getFlush());
+        handsList.add(getStright5());
+        handsList.add(getFlushclubs());
+        handsList.add(getOnepair5());
 
         List<Hands> resultList = evaluator.evalauteHighHands(handsList);
 
+        System.out.println(handsList);
+
         assertThat(resultList.get(0).getHandsType(), is(HandsType.FLUSH));
         assertThat(resultList.get(1).getHandsType(), is(HandsType.STRIGHT));
+        assertThat(resultList.get(2).getHandsType(), is(HandsType.ONE_PAIR));
+    }
+
+    @Test
+    //원페어 세개가 있을 때 그 수를 비교
+    public void evaluateHighCardOnepair() {
+        List<Hands> handsList = new ArrayList<>();
+        handsList.add(getOnepair7());
+        handsList.add(getOnepair10());
+        handsList.add(getOnepair5());
+
+
+        List<Hands> resultList = evaluator.evalauteHighHands(handsList);
+
+        System.out.println(handsList);
+
+        assertThat(resultList.get(0).getCardList().get(0).getNumber(), is(10));
+        assertThat(resultList.get(1).getCardList().get(0).getNumber(), is(7));
+        assertThat(resultList.get(2).getCardList().get(0).getNumber(), is(5));
+    }
+
+    @Test
+    //아무것도 없을 때 가장 큰 수 비교
+    public void evaluateHighCardNothing() {
+        List<Hands> handsList = new ArrayList<>();
+        handsList.add(getNothing9());
+        handsList.add(getNothing10());
+
+
+        List<Hands> resultList = evaluator.evalauteHighHands(handsList);
+
+        System.out.println(handsList);
+
+        assertThat(resultList.get(0).getCardList().get(0).getNumber(), is(10));
+        assertThat(resultList.get(1).getCardList().get(0).getNumber(), is(9));
+    }
+
+    @Test
+    //같을 때 무늬로 비교
+    public void evaluateHighCardFlush() {
+        List<Hands> handsList = new ArrayList<>();
+        handsList.add(getFlushclubs());
+        handsList.add(getFlushspades());
+
+
+        List<Hands> resultList = evaluator.evalauteHighHands(handsList);
+
+        System.out.println(handsList);
+
+        assertThat(resultList.get(0).getCardList().get(0).getSuit(), is(Suit.SPADES));
+        assertThat(resultList.get(1).getCardList().get(0).getSuit(), is(Suit.CLUBS));
+
+    }
+    @Test
+    //같을 때 5개의 수 정렬 중 큰 수를 비교
+    public void evaluateHighCardStraight() {
+        List<Hands> handsList = new ArrayList<>();
+        handsList.add(getStright5());
+        handsList.add(getStright9());
+
+
+        List<Hands> resultList = evaluator.evalauteHighHands(handsList);
+
+        System.out.println(handsList);
+
+        assertThat(resultList.get(0).getCardList().get(0).getNumber(), is(9));
+        assertThat(resultList.get(1).getCardList().get(0).getNumber(), is(5));
+    }
+
+    @Test
+    //같을 때 3개인 숫자를 비교 하여 정렬
+    public void evaluateHighCardFullhouse() {
+        List<Hands> handsList = new ArrayList<>();
+        handsList.add(getFullhouse3());
+        handsList.add(getFullhouse10());
+
+
+        List<Hands> resultList = evaluator.evalauteHighHands(handsList);
+
+        System.out.println(handsList);
+
+        assertThat(resultList.get(0).getCardList().get(0).getNumber(), is(10));
+        assertThat(resultList.get(1).getCardList().get(3).getNumber(), is(3));
     }
 
     @Test
@@ -69,8 +157,8 @@ public class EvaluatorTest {
     @Test
     public void evaluateLowCard1() {
         List<Hands> handsList = new ArrayList<>();
-        handsList.add(getStright());
-        handsList.add(getFlush());
+        handsList.add(getStright5());
+        handsList.add(getFlushclubs());
 
         List<Hands> resultList = evaluator.evalauteLowHands(handsList);
         System.out.print(handsList);
@@ -86,7 +174,29 @@ public class EvaluatorTest {
 
 
     // Givens
-    private Hands getFlush() {
+    private Hands getNothing9() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.CLUBS));
+        cardList.add(new Card(3, Suit.DIAMONDS));
+        cardList.add(new Card(5, Suit.SPADES));
+        cardList.add(new Card(9, Suit.CLUBS));
+        cardList.add(new Card(7, Suit.HEARTS));
+
+        return new Hands(HandsType.NOTHING, cardList);
+    }
+
+    private Hands getNothing10() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.CLUBS));
+        cardList.add(new Card(3, Suit.DIAMONDS));
+        cardList.add(new Card(5, Suit.SPADES));
+        cardList.add(new Card(10, Suit.CLUBS));
+        cardList.add(new Card(7, Suit.HEARTS));
+
+        return new Hands(HandsType.NOTHING, cardList);
+    }
+
+    private Hands getFlushclubs() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(1, Suit.CLUBS));
         cardList.add(new Card(2, Suit.CLUBS));
@@ -97,7 +207,18 @@ public class EvaluatorTest {
         return new Hands(HandsType.FLUSH, cardList);
     }
 
-    private Hands getStright() {
+    private Hands getFlushspades() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(1, Suit.SPADES));
+        cardList.add(new Card(2, Suit.SPADES));
+        cardList.add(new Card(5, Suit.SPADES));
+        cardList.add(new Card(6, Suit.SPADES));
+        cardList.add(new Card(7, Suit.SPADES));
+
+        return new Hands(HandsType.FLUSH, cardList);
+    }
+
+    private Hands getStright5() {
         List<Card> cardList = new ArrayList<>();
         cardList.add(new Card(1, Suit.CLUBS));
         cardList.add(new Card(2, Suit.DIAMONDS));
@@ -115,6 +236,71 @@ public class EvaluatorTest {
         cardList.add(new Card(3, Suit.HEARTS));
         cardList.add(new Card(9, Suit.SPADES));
 
+
+    private Hands getStright9() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(5, Suit.CLUBS));
+        cardList.add(new Card(6, Suit.DIAMONDS));
+        cardList.add(new Card(9, Suit.CLUBS));
+        cardList.add(new Card(8, Suit.HEARTS));
+        cardList.add(new Card(7, Suit.CLUBS));
+
+        return new Hands(HandsType.STRIGHT, cardList);
+    }
+
+    private Hands getOnepair7() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(7, Suit.CLUBS));
+        cardList.add(new Card(5, Suit.DIAMONDS));
+        cardList.add(new Card(7, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(2, Suit.SPADES));
+
+        return new Hands(HandsType.ONE_PAIR, cardList);
+    }
+
+    private Hands getOnepair10() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(10, Suit.CLUBS));
+        cardList.add(new Card(5, Suit.DIAMONDS));
+        cardList.add(new Card(10, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(9, Suit.SPADES));
+
+        return new Hands(HandsType.ONE_PAIR, cardList);
+    }
+
+    private Hands getOnepair5() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(5, Suit.CLUBS));
+        cardList.add(new Card(5, Suit.DIAMONDS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(1, Suit.HEARTS));
+        cardList.add(new Card(2, Suit.SPADES));
+
+        return new Hands(HandsType.ONE_PAIR, cardList);
+    }
+
+
+    private Hands getFullhouse3() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(8, Suit.CLUBS));
+        cardList.add(new Card(8, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.HEARTS));
+        cardList.add(new Card(3, Suit.SPADES));
+        cardList.add(new Card(3, Suit.DIAMONDS));
+
+        return new Hands(HandsType.FULL_HOUSE, cardList);
+    }
+
+    private Hands getFullhouse10() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(10, Suit.HEARTS));
+        cardList.add(new Card(10, Suit.CLUBS));
+        cardList.add(new Card(10, Suit.SPADES));
+        cardList.add(new Card(3, Suit.SPADES));
+        cardList.add(new Card(3, Suit.HEARTS));
+=======
         return new Hands(HandsType.ONE_PAIR, cardList);
     }
 
@@ -145,6 +331,7 @@ public class EvaluatorTest {
         cardList.add(new Card(1, Suit.HEARTS));
         cardList.add(new Card(3, Suit.HEARTS));
         cardList.add(new Card(9, Suit.SPADES));
+
 
         return new Hands(HandsType.FULL_HOUSE, cardList);
     }
